bf3f55a85fac0fafa03e808c114bced3d36db05d
Pankaj Kumar <pankaj151290mech@gmail.com>
2021-01-30 19:55:33 +0530

    check env 22

--- helpers/baseUrl.js
+++ helpers/baseUrl.js
@@ -1,6 +1,8 @@
 const baseUrl =
-  process.env.NODE_ENV === 'production'
+  process.env.NODE_ENV === 'development'
     ? 'https://next-ecom-delta.vercel.app'
     : 'http://localhost:3000';
+
+console.log('================================', baseUrl);
 
 export default baseUrl;

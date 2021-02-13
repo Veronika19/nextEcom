fe5f1ec79281993d12471094119b6f9ab4f29972
Pankaj Kumar <pankaj151290mech@gmail.com>
2021-02-14 01:15:17 +0530

    added support to create product

--- pages/api/product.js
+++ pages/api/product.js
@@ -2,13 +2,40 @@
 import initDB from '../../helpers/initDB';
 import Product from '../../models/Product';
 
+import validCreateProduct from './validations/createProduct';
+
 initDB();
 
 export default async (req, res) => {
+  switch (req.method) {
+    case 'GET':
+      await getAllProducts(req, res);
+      break;
+
+    case 'POST':
+      await createProduct(req, res);
+      break;
+  }
+};
+
+async function getAllProducts(req, res) {
   try {
     const products = await Product.find();
     return res.status(200).json(products);
   } catch (err) {
     console.log(err);
   }
-};
+}
+
+async function createProduct(req, res) {
+  const reqData = req.body;
+  const { errors, isValid } = validCreateProduct(reqData);
+  // check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const { name, price, description, mediaUrl } = req.body;
+  const saveData = await new Product({ name, price, description, mediaUrl }).save();
+  return res.status(200).json(saveData);
+}
--- pages/create.js
+++ pages/create.js
@@ -1,11 +1,16 @@
 import Head from 'next/head';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
+import axios from 'axios';
+
+import baseUrl from '../helpers/baseUrl';
 
 const schema = yup.object().shape({
-  lastname: yup.string().required(),
-  age: yup.string().required(),
+  name: yup.string().required(),
+  price: yup.string().required(),
+  description: yup.string().required(),
   picture: yup.mixed().test('fileSize', 'The file is too large', (value) => {
     if (!value.length) return false; // attachment is optional
     return value[0].size <= 2000000;
@@ -13,11 +18,47 @@ const schema = yup.object().shape({
 });
 
 const Create = () => {
-  const { register, handleSubmit, errors } = useForm({
+  const [media, setMedia] = useState('');
+  const { register, handleSubmit, errors, reset } = useForm({
     resolver: yupResolver(schema),
   }); // initialize the hook
-  const onSubmit = (data) => {
-    console.log(data);
+
+  const onSubmit = async (data) => {
+    const mediaUrl = await imageUpload();
+    const { name, price, description } = data;
+    try {
+      const res = await axios.post(`${baseUrl}/api/product`, {
+        name,
+        price,
+        description,
+        mediaUrl,
+      });
+      reset();
+      setMedia('');
+      M.toast({ html: 'Product saved sucessfully', classes: 'green' });
+    } catch (err) {
+      let html = '';
+      const errData = err.response.data;
+      for (const prop in errData) {
+        html += `<p>${errData[prop]}</p>`;
+      }
+      console.log(err.response.data);
+      M.toast({ html: `<div>${html}</div>`, classes: 'red' });
+    }
+  };
+
+  const imageUpload = async () => {
+    const data = new FormData();
+    data.append('file', media);
+    data.append('upload_preset', 'nxtecomstore');
+    data.append('cloud_name', 'nxtpwp');
+    const res = await fetch(`${process.env.NEXT_PUBLIC_CLOUDINARY_API_BASE_URL}/image/upload`, {
+      method: 'POST',
+      body: data,
+    });
+    const uploadRes = await res.json();
+    console.log(uploadRes);
+    return uploadRes.eager[0].secure_url;
   };
 
   return (
@@ -27,21 +68,37 @@ const Create = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <form onSubmit={handleSubmit(onSubmit)} className="container">
-        <input name="firstname" ref={register} /> {/* register an input */}
-        <input name="lastname" ref={register({ required: true })} />
-        {errors.lastname && 'Last name is required.'}
+        <input name="name" ref={register({ required: true })} />
+        {errors.name && 'Product Name is required.'}
         <div className="file-field input-field">
           <div className="btn">
             <span>File</span>
-            <input ref={register} type="file" name="picture" />
+            <input
+              ref={register}
+              onChange={(e) => setMedia(e.target.files[0])}
+              type="file"
+              name="picture"
+            />
           </div>
           <div className="file-path-wrapper">
             <input className="file-path validate" type="text" />
           </div>
           {errors.picture && <p>{'Please select an image.'}</p>}
+          {media && (
+            <img
+              width="150"
+              height="200"
+              className="responsive-img"
+              src={media ? URL.createObjectURL(media) : ''}
+            />
+          )}
         </div>
-        <input name="age" ref={register({ pattern: /\d+/ })} />
-        {errors.age && 'Please enter number for age.'}
+        <input name="price" ref={register({ pattern: /\d+/ })} />
+        {errors.price && 'Please enter the price.'}
+        <div className="input-field col s12">
+          <input name="description" ref={register({ required: true })} />
+          {errors.description && 'Please enter the description.'}
+        </div>
         <input type="submit" className="btn" />
       </form>
     </div>
